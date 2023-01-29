de0e8b3e26069f2a204b07a04816e98f9e967b7a
jignyasamishra <85229187+jignyasamishra@users.noreply.github.com>
2023-01-29 17:58:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Loadscreen from './Component/Loadscreen';
+
+function App() {
+  return (
+  <>
+<Loadscreen/>
+  </>
+  );
+}
+
+export default App;
---
+++ src/Component/Loadscreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Loadscreen() {
+  return (
+    <div>
+      <div id="loadbody">
+        Jignyasa Mishra's Portfolio
+      </div>
+    </div>
+  )
+}
